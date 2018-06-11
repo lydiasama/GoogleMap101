473b67010977445b5883570e614c39e25610158d
lydiasama <hallelujah.lydia@gmail.com>
2018-06-12 03:00:50 +0700

    permission and get current location

--- app/src/main/java/com/hallelujah/myfirstmap/MainActivity.kt
+++ app/src/main/java/com/hallelujah/myfirstmap/MainActivity.kt
@@ -1,12 +1,78 @@
 package com.hallelujah.myfirstmap
 
+import android.app.Activity
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val RESULT_SELECT_IMAGE = 100
+        const val MEDIA_TYPE_IMAGE = 1
+        const val TAG = "GalleryUtil"
+    }
+
+    var mCurrentPhotoPath = ""
+    var photoFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        try {
+            //Pick Image From Gallery
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, RESULT_SELECT_IMAGE)
+        } catch (e: Exception) {
+            e.printStackTrace();
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            RESULT_SELECT_IMAGE -> {
+                if (resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+                    try{
+                        val selectedImage : Uri = data.data
+                        val filePathColumn = arrayOf( MediaStore.Images.Media.DATA )
+                        val cursor : Cursor = contentResolver.query(
+                                selectedImage,
+                                filePathColumn,
+                                null,
+                                null,
+                                null
+                        )
+                        cursor.moveToFirst()
+                        val columnIndex : Int = cursor.getColumnIndex(filePathColumn[0])
+                        val picturePath = cursor.getString(columnIndex)
+                        cursor.close()
+
+                        //return Image Path to the Main Activity
+                        val returnFromGalleryIntent = Intent()
+                        returnFromGalleryIntent.putExtra("picturePath",picturePath)
+                        setResult(RESULT_OK,returnFromGalleryIntent)
+                        finish()
+                    }catch(e : Exception){
+                        e.printStackTrace()
+                        val returnFromGalleryIntent = Intent()
+                        setResult(RESULT_CANCELED, returnFromGalleryIntent)
+                        finish()
+                    }
+                }else{
+                    Log.i(TAG,"RESULT_CANCELED")
+                    val returnFromGalleryIntent = Intent()
+                    setResult(RESULT_CANCELED, returnFromGalleryIntent)
+                    finish()
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/com/hallelujah/myfirstmap/MapsActivity.kt
+++ app/src/main/java/com/hallelujah/myfirstmap/MapsActivity.kt
@@ -1,43 +1,147 @@
 package com.hallelujah.myfirstmap
 
-import android.support.v7.app.AppCompatActivity
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
-
+import android.os.Looper
+import android.support.annotation.StringRes
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import permissions.dispatcher.*
+import com.google.android.gms.maps.model.Marker
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+@RuntimePermissions
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var mLocationRequest: LocationRequest
+    private lateinit var mLastLocation: Location
+    private lateinit var mCurrLocationMarker: Marker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
+        setupMapFragment()
+
+        btnCurrentLocation.setOnClickListener(this)
+        btnDirection.setOnClickListener(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+    fun setupMapFragment() {
+        mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    override fun onClick(p0: View) {
+        when (p0.id) {
+            R.id.btnCurrentLocation -> {
+                getMyLocationWithPermissionCheck()
+            }
+            R.id.btnDirection -> {
+                Toast.makeText(this, "btnDirection", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mLocationRequest = LocationRequest()
+        mLocationRequest.setInterval(120000) // two minute interval
+        mLocationRequest.setFastestInterval(120000)
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+
+    }
+
+    @NeedsPermission(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun getMyLocation() {
+//        fusedLocationClient.lastLocation
+//                .addOnSuccessListener { location: Location? ->
+//                    location?.let {
+//                        val myLocation = LatLng(location.latitude, location.longitude)
+//                        mMap.addMarker(MarkerOptions().position(myLocation).title("I'm HERE!!"))
+//                        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation))
+//                        mapFragment.getMapAsync(this)
+//                    } ?: Toast.makeText(this, "NULL", Toast.LENGTH_SHORT).show()
+//                }
+
+        fusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+        mMap.setMyLocationEnabled(true);
+    }
+
+    @OnShowRationale(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun showRationaleForLocation(request: PermissionRequest) {
+        showRationaleDialog(R.string.permission_location_rationale, request)
+    }
+
+    @OnPermissionDenied(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun onLocationDenied() {
+        Toast.makeText(this, R.string.permission_location_denied, Toast.LENGTH_SHORT).show()
+    }
+
+    @OnNeverAskAgain(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun onLocationNeverAskAgain() {
+        Toast.makeText(this, R.string.permission_location_never_ask_again, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    var mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList: List<Location> = locationResult.locations
+            if (locationList.isNotEmpty()) {
+                //The last location in the list is the newest
+                val location: Location = locationList.get(locationList.size - 1)
+                Log.i("MapsActivity", "Location: " + location.latitude + " " + location.longitude)
+                mLastLocation = location
+                if (mCurrLocationMarker != null) {
+                    mCurrLocationMarker.remove()
+                }
+
+                //Place current location marker
+                val latLng = LatLng(location.latitude, location.longitude)
+                val markerOptions = MarkerOptions()
+                markerOptions.position(latLng)
+                markerOptions.title("Current Position")
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))
+                mCurrLocationMarker = mMap.addMarker(markerOptions)
+
+                //move map camera
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11F))
+            }
+        }
+    }
+
+    private fun showRationaleDialog(@StringRes messageResId: Int, request: PermissionRequest) {
+        AlertDialog.Builder(this)
+                .setPositiveButton(R.string.button_allow) { _, _ -> request.proceed() }
+                .setNegativeButton(R.string.button_deny) { _, _ -> request.cancel() }
+                .setCancelable(false)
+                .setMessage(messageResId)
+                .show()
     }
 }
